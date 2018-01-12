10d7335ec51e9022021ffdab4133a955774df362
BenjaminLL <854623230@qq.com>
2018-01-12 11:12:12 -0500

    assignment

---
+++ subscribe.js
@@ -0,0 +1,20 @@
+
+
+$("#signup").on("click", function() {
+	var str = $("#email").val();
+	var valid = false;
+	
+	for (var i = 0; i < str.length; ++i) {
+		if (str[i] === "@") {
+			valid = true;
+		}
+	}
+
+	if (!valid) {
+		$("#errorMessage").text("Please enter a valid email address");
+	} else {
+		console.log("Email Address: " + $("#email").val());
+		console.log("Interested in: " + $("#type").val());
+		$("#errorMessage").text("");
+	}
+}) 
